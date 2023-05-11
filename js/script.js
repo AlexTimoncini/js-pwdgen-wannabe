a34db03c750ce8b32392c3216218a893f36ab4ac
AlexTimoncini <alextimonci@gmail.com>
2023-05-11 14:39:17 +0200

    added script to update user info inside the html

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const userName = prompt('Inserisci qui il tuo nome')
+const userSurname = prompt('Inserisci qui il tuo cognome')
+const UserColor = prompt('Inserisci qui il tuo colore preferito')
+
+document.getElementById('person_name').innerHTML = userName
+document.getElementById('person_surname').innerHTML = userSurname
+document.getElementById('person_color').innerHTML = UserColor
