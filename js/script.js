c356c311f663dd589a2e614247e6dc53cce93776
AlexTimoncini <alextimonci@gmail.com>
2023-05-12 12:21:29 +0200

    excersi corrected using template literals

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
 const userName = prompt('Inserisci qui il tuo nome')
 const userSurname = prompt('Inserisci qui il tuo cognome')
-const UserColor = prompt('Inserisci qui il tuo colore preferito')
+const userColor = prompt('Inserisci qui il tuo colore preferito')
 
-document.getElementById('person_name').innerHTML = userName
-document.getElementById('person_surname').innerHTML = userSurname
-document.getElementById('person_color').innerHTML = UserColor
+document.getElementById('userPassword').innerHTML = `${userName + userSurname + userColor + '23'}`
